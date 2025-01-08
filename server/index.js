faa370d4625df3696c9c9f879dac300611e6e88c
Ben <sitta@gmail.com>
2025-01-08 23:50:46 +0100

    Initial commit: Added Budget Tracker project

--- server/index.js
+++ server/index.js
@@ -2,83 +2,90 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Record = require('./model/blog');
 const Contact = require('./model/contact');
+require('dotenv').config(); // Import and configure dotenv
 
 const app = express();
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-//mongo db
-const dbURI = 'mongodb+srv://sittafrfr:test1234@mongotypeshi.l1ycl.mongodb.net/notes?retryWrites=true&w=majority&appName=MongoTypeShi';
+// Use the environment variable for the MongoDB connection
+const dbURI = process.env.MONGO_LINK;
+
 mongoose.connect(dbURI)
-    .then((result)=>app.listen(3000))
-    .catch((err)=>console.log(err));
+    .then((result) => app.listen(3000, () => console.log('Server is running on port 3000')))
+    .catch((err) => console.log(err));
 
+// Routes
+app.get('/', async (req, res) => {
+    try {
+        const latestRecord = await Record.findOne().sort({ createdAt: -1 });
+        const totalSpending = await Record.aggregate([{ $group: { _id: null, total: { $sum: "$amount" } } }]);
+        const categoryTotals = await Record.aggregate([{ $group: { _id: "$cat", total: { $sum: "$amount" } } }]);
 
+        res.render('home', {
+            title: 'Home page',
+            record: latestRecord,
+            totalSpending: totalSpending[0] ? totalSpending[0].total : 0,
+            categoryTotals
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
 
+app.get('/add', (req, res) => {
+    res.render('add', { title: 'Add Record' });
+});
 
-//getting diff types of req to access pages
-app.get('/',(req,res)=>{
-    res.render('home',{title: 'Home page'});
-})
-app.get('/add',(req,res)=>{
-    res.render('add',{title: 'Add Record'});
-})
-// app.get('/history',(req,res)=>{
-//     res.render('history',{title: 'Spending History'});
-// })
-app.get('/update',(req,res)=>{
-    res.render('update',{title: 'Updating Information'});
-})
-app.get('/contact',(req,res)=>{
-    res.render('contact',{title: 'Contact Us'});
-})
+app.get('/connect', (req, res) => {
+    res.render('connect', { title: 'Connect with Us' });
+});
 
-//getting data from submit btn
-app.post('/add',(req, res)=>{
-    //console.log(req.body);
+app.post('/add', (req, res) => {
     const newRecord = new Record(req.body);
     newRecord.save()
-       .then((result)=>{
-           res.redirect('/add');
-       })
-       .catch((err)=>{
-           console.log(err);
-       })
-})
-app.post('/contact',(req, res)=>{
-    const newContact = new Contact(req.body);
-    newContact.save()
-       .then((result)=>{
-           res.redirect('/contact');
-       })
-       .catch((err)=>{
-           console.log(err);
-       })
-})
-
-
-//trial
-// app.get('/all',(req,res)=>{
-//     Record.find()
-//         .then((result)=>{
-//             res.send(result);
-//         })
-//         .catch((err)=>{
-//             console.log(err)
-//         })
-// })
-//showing spending history
-
-app.get('/history', (req, res) => {
-    Record.find().sort({ createdAt: -1 })
         .then((result) => {
-            res.render('history', { show: result, title: 'Spending history' });        })
+            res.redirect('/add');
+        })
         .catch((err) => {
             console.log(err);
         });
 });
 
-app.use((req,res)=>{
-    res.status(404).render('404', {title: '404'});
-})
+app.post('/connect', (req, res) => {
+    const newContact = new Contact(req.body);
+    newContact.save()
+        .then((result) => {
+            res.redirect('/connect');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.delete('/blogs/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Record.findByIdAndDelete(id);
+        res.json({ redirect: '/history' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error deleting record");
+    }
+});
+
+app.get('/history', (req, res) => {
+    Record.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('history', { show: result, title: 'Spending history' });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// Handle 404 errors
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
